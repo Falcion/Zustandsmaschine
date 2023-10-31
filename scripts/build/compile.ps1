549d6b4e830d04066cad323794bb416ed04d039b
Falcion <falcionml@gmail.com>
2023-10-31 08:03:31 +0300

    REPO CLEAR: global refactoring

--- scripts/build/compile.ps1
+++
@@ -1,9 +0,0 @@
-$projectPath = "./../../source/Zustand.csproj"
-$outputDir = "./../../source/build"
-
-# Clean and build the project as a DLL
-dotnet clean $projectPath --configuration Release
-dotnet build $projectPath --configuration Release --output $outputDir
-
-# Publish the DLL as a NuGet package
-dotnet pack $projectPath --configuration Release --output $outputDir
