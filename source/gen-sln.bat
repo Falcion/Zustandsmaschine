20343bbf6aab4082a439084dc16987973255ceba
Falcion <falcionml@gmail.com>
2023-11-09 16:42:52 +0300

    release(core): add support for .NET for future coding
    - Including workflows with .NET custom build and CodeQL integration;
    - And scripts with .CSPROJ files;

---
+++ source/gen-sln.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Get the .csproj file name
+for %%i in (*.csproj) do set "csproj_file=%%~ni"
+
+REM Generate the .sln file
+dotnet new sln --name Zustand
+
+REM Add the project to the solution
+dotnet sln add "%csproj_file%".csproj
